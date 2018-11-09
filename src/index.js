2610caa6e552070fc15d8589eb0c2e05afb9dcd1
Dimitris Voudrias <d.voudrias@sieben.gr>
2018-11-09 21:12:56 +0200

    movies styles implemented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'fontawesome/index.js';
+import 'font-awesome/css/font-awesome.css';
 import './styles/styles.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
