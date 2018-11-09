b52f7041353f4e63c727fdb87ea16692b2e8afcd
Dimitris Voudrias <d.voudrias@sieben.gr>
2018-11-09 20:52:42 +0200

    toolbar styles implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'fontawesome/index.js';
 import './styles/styles.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
